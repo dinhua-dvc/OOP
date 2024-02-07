5066ab9f931958d3ce5052bf7e9b37fc894cf6fa
Joshua Slui <joshuaslui0203@gmail.com>
2024-02-07 18:00:05 +0100

    Add class for Charmander with constructor to super() class

---
+++ src/Charmander.java
@@ -0,0 +1,5 @@
+public class Charmander extends Pokemon {
+    public Charmander(String name, String type, String strength, String weakness) {
+        super(name, type, strength, weakness);
+    }
+}
