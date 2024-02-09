844524fd4fd2d5607cd08b0c9b876b1568b8035f
Joshua Slui <joshuaslui0203@gmail.com>
2024-02-09 09:14:01 +0100

    Only super the name in charmander, override Pokemon getters

--- src/Charmander.java
+++ src/Charmander.java
@@ -1,5 +1,20 @@
 public class Charmander extends Pokemon {
-    public Charmander(String name, String type, String strength, String weakness) {
-        super(name, type, strength, weakness);
+    public Charmander(String name) {
+        super(name);
+    }
+
+    @Override
+    Powers getType() {
+        return Powers.FIRE;
+    }
+
+    @Override
+    Powers getStrength() {
+        return Powers.FIRE;
+    }
+
+    @Override
+    Powers getWeakness() {
+        return Powers.WATER;
     }
 }
