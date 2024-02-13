60461962ac658038c9623726ed363c94bdd82684
MilanoGamezzYT <99067157@mydavinci.nl>
2024-02-13 12:41:25 +0100

    Made the pokeball.java

--- src/Pokeball.java
+++ src/Pokeball.java
@@ -1,2 +1,25 @@
 public class Pokeball {
+    private String name;
+    private Pokemon pokemon;
+
+    public Pokeball(String name, Pokemon pokemon) {
+        this.name = name;
+        this.pokemon = pokemon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Pokemon getPokemon() {
+        return pokemon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPokemon(Pokemon pokemon) {
+        this.pokemon = pokemon;
+    }
 }
--- src/Trainer.java
+++ src/Trainer.java
@@ -1,2 +1,4 @@
 public class Trainer {
+    public Trainer(String name);
+    
 }
